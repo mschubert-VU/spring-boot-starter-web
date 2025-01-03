f4c4d4b7bfde815e651a3f603767fc0f7edb26f7
Your Name <you@example.com>
2025-01-03 13:50:17 +0100

    Change welcome string to create new docker image tag

--- app/src/main/java/com/example/demo/Application.java
+++ app/src/main/java/com/example/demo/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World v4";
+		return "Hello SpringBoot";
 	}
 
 	public static void main(String[] args) {
